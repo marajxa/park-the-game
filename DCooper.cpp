a08851c83b7ba5c070f5fa86249eb7a8ed62c191
marajxa <smrehrig@gmail.com>
2017-05-30 14:13:01 -0700

    made case flexible in dialog with cooper

--- DCooper.cpp
+++ DCooper.cpp
@@ -31,22 +31,22 @@ int DCooper::talk(Inventory* i){
   cout << "C: I have a question about my project " << endl;
 
   cin >> into;
-  if (into == "A"){
+  if (into == "A" || into == "a"){
     cout << name << ": " << "Well, if you need anything I'm here" << endl;
     cout << " " << endl;
     cout << "MOTIVATION STAYS THE SAME" << endl;
     return 0;
-  } else if(into == "B"){
+  } else if(into == "B" || into == "b"){
     cout << name << ": " << "A key to what?" << endl;
     cout << "A: Any key really" << endl;
     cout << "B: A key to the lab" << endl;
     cin >> into2;
-    if(into2 == "A"){
+    if(into2 == "A" || into2 == "a"){
       cout << name << ": " << "Well, I can't just give you any key" << endl;
       cout << " " << endl;
       cout << "LOST 5 MOTIVATION" << endl;
       return 0;
-    }else if(into == "B"){
+    }else if(into2 == "B" || into2 == "b"){
       cout << name << ": " << "Why do you need a key to the lab?" << endl;
       cout << " " << endl;
       cout << "LOST 2 MOTIVATION" << endl;
@@ -56,12 +56,12 @@ int DCooper::talk(Inventory* i){
     }else{
       cout << "invalid input" << endl;
     }
-  }else if(into == "C"){
+  }else if(into == "C" || into == "c"){
     cout << name << ": " << "I have an answer" << endl;
     cout << "A: If we go to the lab I'll show you my problem" << endl;
     cout << "B: nevermind" << endl;
     cin >> into3;
-    if (into3 == "A"){
+    if (into3 == "A" || into3 == "a"){
       cout << name << "; " << "Sure, let's go" << endl;
 
       Key* key = static_cast<Key*>(inventory->getFromPos(1));
@@ -72,7 +72,7 @@ int DCooper::talk(Inventory* i){
       cout << " " << endl;
       cout << "GAIN  5 MOTIVATION" << endl;
       return 5;
-    }else if (into3 == "B"){
+    }else if (into3 == "B" or into3 == "b"){
       cout << name << ": " << "Well if you need anything..." << endl;
       cout <<" " << endl;
       cout << "MOTIVATION STAYS THE SAME" << endl;
